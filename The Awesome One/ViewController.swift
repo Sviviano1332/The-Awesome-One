7fd4d46cd27ae039a7c11e41e1aa0f49e0a4d469
Sophia Viviano <sviviano@zagmail.gonzaga.edu>
2019-09-05 16:20:57 -0700

    Initial Commit

---
+++ The Awesome One/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Awesome One
+//
+//  Created by Sophia Viviano on 9/5/19.
+//  Copyright © 2019 Sophia Viviano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
