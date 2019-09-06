05eca6fd72e2324d96dbc420b2b657cff4dcc4eb
Sophia Viviano <sviviano@zagmail.gonzaga.edu>
2019-09-05 23:58:11 -0700

    The Awesome One

--- The Awesome One/ViewController.swift
+++ The Awesome One/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var viewer: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    var index = -1
+    var newIndex = -1
+    var randomStart = Int.random(in: 0...7)
+    
+    var students = ["Ethan",
+                    "Braden",
+                    "Kyle",
+                    "Guy2",
+                    "Guy3",
+                    "Jessica",
+                    "Sophia",
+                    "Guy 4"]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func button(_ sender: UIButton) {
+        
+        if randomStart > students.count-1 {
+            randomStart = 0
+        }
+        label.text = students [randomStart]
+        viewer.image = UIImage (named: "img\(randomStart)")
+        randomStart += 1
+    }
+    
 }
 
